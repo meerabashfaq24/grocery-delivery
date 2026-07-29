ee0cbaa2b730d728df2f4e5d5d65104957a56c1e
Meerab <meerabashfaq24@gmail.com>
2026-07-29 08:16:50 +0500

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://grocery-delivery-38jv.onrender.com/api",
 });
 
 export default api;
